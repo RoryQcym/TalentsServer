a6386198f728c27cb1945890d86187180842f408
rory <ll525549451@sina.com>
2018-05-16 22:46:31 +0800

    “init

---
+++ src/main/java/com/fbc/TalentsServer/TalentsServerApplication.java
@@ -0,0 +1,12 @@
+package com.fbc.TalentsServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentsServerApplication.class, args);
+	}
+}
